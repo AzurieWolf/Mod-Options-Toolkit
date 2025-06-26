b0958d530700388be61348e803ea1ffbaff55a74
Travis Singleton <AzurieWolf@outlook.com>
2025-06-26 15:57:45 -0300

    removed

--- build_mod_option_selector.ps1
+++
@@ -1,7 +0,0 @@
-# Step 1: Build with PyInstaller
-pyinstaller --noconfirm --onefile --clean --windowed --icon=assets/icon.ico --name=Mod_Option_Selector mod_option_selector.py
-
-# Step 2: Copy data folder to dist
-Copy-Item -Recurse -Force data dist\data
-
-Write-Host "Build complete. data/ copied to dist/."
